e61e1c36e2397dd99e801f1213516e577def6088
Jack-Alexman <60382907+Jack-Alexman@users.noreply.github.com>
2020-01-29 17:53:41 +0200

    Commit

--- src/work65/Task65.java
+++ src/work65/Task65.java
@@ -2,6 +2,10 @@ package work65;
 
 public class Task65 {
     public static void printPyramid(int height){
-
+        int i = 0;
+        while (i <= height){
+            
+            i++;
+        }
     }
 }
