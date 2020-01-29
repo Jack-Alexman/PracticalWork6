96d1eab366090ab662fbdcdb8792b48a86b3a42c
Jack-Alexman <60382907+Jack-Alexman@users.noreply.github.com>
2020-01-29 20:47:25 +0200

    Commit

--- src/work65/Task65.java
+++ src/work65/Task65.java
@@ -1,12 +1,29 @@
 package work65;
 
+import java.util.Scanner;
+
 public class Task65 {
     public static void printPyramid(int height){
-        int i = 0;
+        int i = 1;
         while (i <= height){
-            
+            int j = 2 * height - 1;
+            while (j > 0 ){
+                if ((j > (2 * height) / 2 - i) &&
+                        (j < (2 * height) / 2 + i)){
+                    System.out.print("*");
+                } else
+                System.out.print(" ");
+                j--;
+            }
             System.out.println();
             i++;
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите высоту пирамиды => ");
+        int height = sc.nextInt();
+        sc.close();
+        printPyramid(height);
     }
 }
